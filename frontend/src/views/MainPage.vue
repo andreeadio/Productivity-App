7d2ef13a8289ebe4fb89a0ef8c906dceb0ff650c
Andreea Dioclitean <diocliteanandreea21@stud.ase.ro>
2025-02-03 12:48:02 +0200

    modified main

--- frontend/src/views/MainPage.vue
+++ frontend/src/views/MainPage.vue
@@ -1,9 +1,36 @@
+
 <template>
-    <div>
-        <h1>Dashboard </h1>
-        
-
-    </div>
+  <v-container>
+    <v-row>
+      <v-col cols="12">
+        <h1 class="text-center">Focus Session</h1>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12" md="6" offset-md="3">
+        <PomodoroTimer />
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
+  
+<script>
+  import PomodoroTimer from "@/components/PomodoroTimer.vue";
 
-<script></script>
+  export default {
+    components: {
+      PomodoroTimer,
+    },
+    setup() {
+    
+  },
+  };
+  </script>
+  
+  <style scoped>
+  .dashboard {
+    text-align: center;
+    padding: 20px;
+  }
+  </style>
+  
